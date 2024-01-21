24b869ecd4e86ffedc7dadd7b48217745038f176
maxdizy <max@dizy.ca>
2024-01-21 16:43:28 -0500

    model.py

--- testCode/dataHandler.py
+++ testCode/dataHandler.py
@@ -9,24 +9,24 @@ dataFile = pd.read_csv('data/testdata1.csv')
 df = pd.DataFrame(dataFile)
 
 #insert result col
-df['results'] = 0
+df['target'] = 0
 
 #detect tricks
 for i in range(df['FR'].size-2):
     #find start of trick with outlier filter
     if ((inAir == False) & (df['FR'].iloc[i] < airThresh) & (df['FR'].iloc[i + 1] < airThresh) & (df['FR'].iloc[i + 2] < airThresh)):
         trickStart = i
+        #note: this may be an issue if trick detected is before two seconds, size would be less then 12
+        tricks.append(df.iloc[max(0, i-8) : min(i+4, df['FR'].size-2)]) #create trick df with -2000 and +1000 millis
         inAir = True
 
     #find end of trick with outlier filter
     if ((inAir == True) & (df['FR'].iloc[i] > airThresh) & (df['FR'].iloc[i + 1] > airThresh) & (df['FR'].iloc[i + 2] > airThresh)):
-        trickEnd = i
-        tricks.append(df.iloc[max(0, trickStart-4) : min(trickEnd+4, df['FR'].size-2)]) #create trick df with +- 1000 millis
         inAir = False
 
-trickNum = 1
-for trickData in tricks:
-    print('Trick ' + str(trickNum) + ':\n')
-    print(trickData)
-    print('\n')
-    trickNum += 1
+#concat data
+trickData = np.array([np.array(trick.drop('target', axis=1)) for trick in tricks])
+target = np.array([np.array(trick['target']) for trick in tricks])
+
+#shape for model: 3 tricks, 12 steps, 7 sensors
+trickData = trickData.reshape((trickData.shape[0], trickData.shape[1], trickData.shape[2]))
---
+++ testCode/model.py
@@ -0,0 +1,34 @@
+import os
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from keras import layers
+from dataHandler import trickData, target
+
+# define model
+input_size = trickData.shape[2]
+seq_length = trickData.shape[1]
+model = keras.models.Sequential()
+model.add(keras.Input(shape=(seq_length, input_size)))
+# test SimpleRNN, LSTM, and GRU
+model.add(layers.LSTM(50, activation="relu"))
+model.add(layers.Dense(1))
+# print(model.summary())
+
+#  loss and optimizer
+loss = keras.losses.MeanSquaredError()
+optim = keras.optimizers.Adam(learning_rate=0.001)
+metrics = ["accuracy"]
+model.compile(loss=loss, optimizer=optim, metrics=metrics)
+
+# Train model
+# epochs=10 specifies the number of times the training process will work through the entire dataset. One epoch means that each sample in the training dataset has had an opportunity to update the internal model parameters. A higher number of epochs can lead to better training but also risks overfitting if too large.
+# batch_size=32 defines the number of samples that will be propagated through the network before the model's internal parameters are updated. In this case, 32 samples from trickData are processed, and then the model is updated. The choice of batch size can affect the speed and stability of the training process.
+# verbose = 1 means that you will see a progress bar in the output showing the training progress. If set to 0, you would see no output during training, and if set to 2, you would see one line per epoch.
+model.fit(trickData, target, epochs=10, batch_size=32, verbose=1)
+
+# # Evaluate model
+# loss, accuracy = model.evaluate(trickData_test, target_test)
+# print(f'Test accuracy: {accuracy}')
