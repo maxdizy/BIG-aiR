9cea7e3d261baaf09f58a54eeae1537a544b7654
maxdizy <max@dizy.ca>
2024-03-01 19:24:41 -0500

    datahandler and data undated

--- testCode/dataHandler.py
+++ testCode/dataHandler.py
@@ -1,27 +1,57 @@
 import pandas as pd
 import numpy as np
+import math
 
-airThresh = 140
+airThresh = 100
 inAir = False
 tricks = []
 
 dataFile = pd.read_csv('data/BIG_aiR_data.csv')
 df = pd.DataFrame(dataFile)
 
-#insert result col
+# clean dataset
+for i in range(df['RBV'].size-2):
+    RFP = df['RFP'].iloc[i]
+    RBP = float(df['RBP'].iloc[i])
+    RFV = df['RFV'].iloc[i]
+    RBV = df['RBV'].iloc[i]
+    LFP = df['LFP'].iloc[i]
+    LBP = float(df['LBP'].iloc[i])
+    LFV = df['LFV'].iloc[i]
+    LBV = df['LBV'].iloc[i]
+    # check front percentages
+    if math.isnan(RFP):
+        if not math.isnan(float(RBP)):
+            df.loc[i, 'RFP'] = 100 - RBP
+    if math.isnan(LFP):
+        if not math.isnan(float(LBP)):
+            df.loc[i, 'LFP'] = 100 - LBP
+    # check back percentages
+    if math.isnan(RBP):
+        if not math.isnan(float(RFP)):
+            df.loc[i, 'RBP'] = 100 - RFP
+    if math.isnan(LBP):
+        if not math.isnan(float(LFP)):
+            df.loc[i, 'LBP'] = 100 - LFP
+# fill in all missing NaN's
+df = df.fillna(0)
+
+# insert result col
 df['target'] = 0
 
 #detect tricks
-for i in range(df['FR'].size-2):
+for i in range(df['RBV'].size-2):
+    comb = df['RFV'].iloc[i] + df['RBV'].iloc[i]
+
     #find start of trick with outlier filter
-    if ((inAir == False) & (df['FR'].iloc[i] < airThresh) & (df['FR'].iloc[i + 1] < airThresh) & (df['FR'].iloc[i + 2] < airThresh)):
+    if ((inAir == False) & (comb < airThresh)):
         trickStart = i
         #note: this may be an issue if trick detected is before two seconds, size would be less then 12
-        tricks.append(df.iloc[max(0, i-8) : min(i+4, df['FR'].size-2)]) #create trick df with -2000 and +1000 millis
+        tricks.append(df.iloc[max(0, i-4) : min(i+4, df['RBV'].size-2)]) #create trick df with -2000 and +1000 millis
         inAir = True
 
     #find end of trick with outlier filter
-    if ((inAir == True) & (df['FR'].iloc[i] > airThresh) & (df['FR'].iloc[i + 1] > airThresh) & (df['FR'].iloc[i + 2] > airThresh)):
+    if ((inAir == True) & (comb > airThresh)):
         inAir = False
 
 #concat data
